58d2b819ef444214d26abce914527dcb78733d87
abhijeet-Bh <abhijeetbhardwaj53@gmail.com>
2024-08-14 22:51:06 +0530

    feat: initial project setup

---
+++ src/main/java/com/abhijeet/job_site/JobSiteApplication.java
@@ -0,0 +1,13 @@
+package com.abhijeet.job_site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abhijeet/job_site/JobSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhijeet.job_site;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
